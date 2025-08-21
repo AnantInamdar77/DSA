feeed5faba46f60a8e31dacf92789688692db541
Anant Inamdar <anantinamdar77@gmail.com>
2025-08-21 21:22:28 +0530

    introduction to String

---
+++ Strings/Main.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+
+
+        String name="Anant";
+        String b="Anant";
+        name="Inamdar";      //this is not changing the object its creating new object in memory.
+        System.out.println(name);
+        //String:- String are immutable in JAVA
+
+    }
+}
